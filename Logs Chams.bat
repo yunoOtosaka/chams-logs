a5780c6e5003da526b9dc4d16c39a71ccb278230
yuno <107594664+yunoOtosaka@users.noreply.github.com>
2024-12-31 21:20:37 -0300

    Add files via upload

---
+++ Logs Chams.bat
@@ -0,0 +1,5 @@
+@echo off
+rmdir /s /q "C:\Users\Default\AppData\Local\1"
+rmdir /s /q "C:\Users\Default\AppData\Local\2"
+rmdir /s /q "C:\Users\Default\AppData\Local\3"
+rmdir /s /q "C:\Users\Default\AppData\Local\3" 
